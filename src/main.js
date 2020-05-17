402b0c725212109bd9a3792c9483cf571de9a2e3
kalnynsh <kda869@yandex.ru>
2020-05-17 20:05:40 +0300

    Added coomented block to main.js.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ class App {
     run = (name = 'B Sandbox') => {
         console.log(`This App\`s name is "${name}".`);
     };
+
+    /**  Classic version */
+    // constructor() {
+    //     this.run = (name = 'B Sandbox') => {
+    //         console.log(`This App\`s name is "${name}".`);
+    //     };
+    // }
 }
 
 const app = new App();
