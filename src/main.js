597c41a3a1c8f675a34d258dc8bc614351486057
kalnynsh <kda869@yandex.ru>
2020-05-17 20:01:39 +0300

    Added proposal-class-properties feature to main.js.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 
 class App {
 
-    constructor() {
-        this.run = (name = 'B Sandbox') => {
-            console.log(`This App\`s name is "${name}".`);
-        };
-    }
+    run = (name = 'B Sandbox') => {
+        console.log(`This App\`s name is "${name}".`);
+    };
 }
 
 const app = new App();
