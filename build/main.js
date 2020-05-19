8aebfed010a2f1542df17f31fd87ff94a8cabfe8
kalnynsh <kda869@yandex.ru>
2020-05-19 19:18:59 +0300

    Added browserlist constraints to babelrc.

--- build/main.js
+++ build/main.js
@@ -1,12 +1,31 @@
 "use strict";
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 class App {
   constructor() {
-    _defineProperty(this, "run", async (name = 'B Sandbox') => {
-      console.log(`This App\`s name is "${name}".`);
-    });
+    _defineProperty(this, "run", /*#__PURE__*/function () {
+      var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(name = 'B Sandbox') {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) switch (_context.prev = _context.next) {
+            case 0:
+              console.log("This App`s name is \"".concat(name, "\"."));
+
+            case 1:
+            case "end":
+              return _context.stop();
+          }
+        }, _callee);
+      }));
+
+      return function () {
+        return _ref.apply(this, arguments);
+      };
+    }());
   }
   /**  Classic version */
   // constructor() {
