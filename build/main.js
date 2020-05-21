5408bf25d6c495c64f1586472ccd92bed44185bf
kalnynsh <kda869@yandex.ru>
2020-05-21 17:51:00 +0300

    Removed babel main.js.

--- build/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "React Application"), /*#__PURE__*/React.createElement("p", null, "Test text"));
-};
-
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
