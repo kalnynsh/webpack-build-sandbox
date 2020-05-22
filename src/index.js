98fbf1eb86adc9fb3672d52632a0cbd4e21fcf54
kalnynsh <kda869@yandex.ru>
2020-05-22 19:52:56 +0300

    Updated index.js with simple React App.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-import Calculator from './calculator';
-import Logger from './logger';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import logoUrl from './images/logo60x73.png';
-import './images/logo13x16.ico';
-import './images/logo192.png';
-import './images/logo512.png';
+const App = () => {
+    return (
+        <div>
+            <h1>I am the React App</h1>
+            <p>Lorem ipsum.</p>
+        </div>
+    );
+}
 
-const calculator = new Calculator();
-const logger = new Logger();
-
-logger.log(
-    calculator.sum(43,96,71,75)
-);
-
-const logoImg = document.createElement('img');
-logoImg.src = logoUrl;
-
-document
-    .getElementById('root')
-    .appendChild(logoImg)
-;
+ReactDOM.render(<App />, document.getElementById('root'));
