8017a06b1beab72214ea29ea19ba48e1dbd863ec
kalnynsh <kda869@yandex.ru>
2020-05-22 19:27:46 +0300

    Added config for font loading.

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,25 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /.+\.(png|jpg|ico)$/,
+                test: /.+\.(png|jpg|jpeg|gif|ico)$/,
                 use: [
                     {
                         loader: 'file-loader',
                         options: {
                             outputPath: 'images',
                             name: '[name]-[sha1:hash:9].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /.+\.(ttf|otf|eot|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts',
+                            name: '[name].[ext]'
                         }
                     }
                 ]
